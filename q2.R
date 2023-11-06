50d3bc60bf0ee53f07b252bb5be6de88fad50ae4
NAZLIBILGIC <nazli.blgc@gmail.com>
2023-11-06 14:04:56 +0100

    change for question 2

--- q2.R
+++ q2.R
@@ -1,29 +1,32 @@
-library(ggplot2)
+options(digits =22)
+
 myvar <- function(x) {
   n <- length(x)
-  return((sum(x^2) - (sum(x)^2) / n) / (n - 1))
-}
+  a<-sum(x^2)
+  b<-sum(x)
+  c<-(a-(b^2/ n)) / (n - 1)
 
+  return(c)
+}
 set.seed(123)
 n <- 10000
 mean_value <- 10^8
 variance<-1
 std_dev <- sqrt(variance)
-x <- rnorm(n, mean = mean_value, sd = std_dev)
+x <- rnorm(n, mean_value, std_dev)
 
-computed_variance <- myvar(x)
-print(computed_variance)
-
-print(var(x))
-
-difference_fnctns <- numeric(n)
-for (i in 1:n) {
+difference_fnctns <- c()
+for (i in 2:n) {
   x_sub <- x[1:i]
-  difference_fnctns[i] <- myvar(x_sub) - var(x_sub)
+  y_sub <- myvar(x_sub) - var(x_sub)
+  difference_fnctns <- c(difference_fnctns, y_sub)
 }
 
+plot(difference_fnctns)
+
+#print(var(x))
 #print(difference_fnctns)
-plot(1:n, differences, type="l", col="red", main="myvar and R's var function difference", xlab="i", ylab="Yi")
+#plot( difference_fnctns, type="p", col="red", main="myvar and R's var function difference", xlab="i", ylab="Yi")
 
 
 
---
+++ try2.R
@@ -0,0 +1,33 @@
+library(ggplot2)
+
+# Your provided two-pass variance computation function
+var_two_pass <- function(x) {
+  n <- length(x)
+  mean_val <- sum(x) / n
+  sum_sq_diffs <- sum((x - mean_val)^2)
+  return(sum_sq_diffs / (n - 1))
+}
+
+set.seed(123)
+n <- 10000
+mean_value <- 10^8
+variance <- 1
+std_dev <- sqrt(variance)
+
+num_datasets <- 100
+differences <- numeric(num_datasets)
+
+# Compute differences for multiple datasets
+for (i in 1:num_datasets) {
+  x <- rnorm(n, mean = mean_value, sd = std_dev)
+  differences[i] <- var_two_pass(x) - var(x)
+}
+
+# Plot the differences
+df <- data.frame(dataset = 1:num_datasets, difference = differences)
+ggplot(df, aes(x = dataset, y = difference)) +
+  geom_point() +
+  theme_minimal() +
+  labs(title = "Difference between var_two_pass and R's var function",
+       x = "Dataset number",
+       y = "Difference in variance")
